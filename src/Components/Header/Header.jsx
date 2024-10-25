63dcc06537d7591f402ec48390bc077359a85122
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-10-25 17:50:50 +0300

    salomlar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,28 +4,33 @@ import Rasm_1 from "../images/Plant.png"
 const Header = () => {
   return (
     <>
-        <div className=''>
-           <div className='max-w-[1200px] mx-auto p-[20px] flex justify-between'>
-                <img className='' src={Rasm_1} alt="" />
-                <ul class="flex">
-                    <li class="">
-                        <a className='font-dm-sans text-[16px] font-normal leading-[20.83px]' href="">Home</a>
-                    </li>
-                    <li>
-                        <a className='font-dm-sans text-[16px] font-normal leading-[20.83px]' href="">Services</a>
-                    </li>
-                    <li>
-                        <a className='font-dm-sans text-[16px] font-normal leading-[20.83px]' href="">About Us</a>
-                    </li>
-                    <li>
-                        <a className='font-dm-sans text-[16px] font-normal leading-[20.83px]' href="">Blog</a>
-                    </li>
-                    <li>
-                        <a className='font-dm-sans text-[16px] font-normal leading-[20.83px]' href="">Contact</a>
-                    </li>
-                </ul>
-           </div>
+      <div className=''>
+        <div className='max-w-[1200px] mx-auto p-[20px] flex justify-between'>
+          <ul className=''>
+             <img className='' src={Rasm_1} alt="Plant logo" />
+          </ul>
+          <ul className="flex list-none space-x-8">
+            <li>
+              <a className='font-dm-sans text-[16px] font-normal leading-[20.83px]' href="">Home</a>
+            </li>
+            <li>
+              <a className='font-dm-sans text-[16px] font-normal leading-[20.83px]' href="">Services</a>
+            </li>
+            <li>
+              <a className='font-dm-sans text-[16px] font-normal leading-[20.83px]' href="">About Us</a>
+            </li>
+            <li>
+              <a className='font-dm-sans text-[16px] font-normal leading-[20.83px]' href="">Blog</a>
+            </li>
+            <li>
+              <a className='font-dm-sans text-[16px] font-normal leading-[20.83px]' href="">Contact</a>
+            </li>
+          </ul>
+          <ul className=''>
+
+          </ul>
         </div>
+      </div>
     </>
   )
 }
