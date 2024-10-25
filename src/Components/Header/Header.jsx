5597cfaffa23d988ecaa0fbf21c610d316100d08
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-10-25 17:38:03 +0300

    salomlar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,17 +7,27 @@ const Header = () => {
         <div className=''>
            <div className='max-w-[1200px] mx-auto p-[20px] flex justify-between'>
                 <img className='' src={Rasm_1} alt="" />
-                <ul className=''>
-                    <li className='w-[44px] h-[19.42px] font-dm-sans text-[16px] font-normal leading-[20.83px] text-left'><a href="">Home</a></li>
-                    <li className='w-[44px] h-[19.42px] font-dm-sans text-[16px] font-normal leading-[20.83px] text-left'><a href="">Services</a></li>
-                    <li className='w-[44px] h-[19.42px] font-dm-sans text-[16px] font-normal leading-[20.83px] text-left'><a href="">About Us</a></li>
-                    <li className='w-[44px] h-[19.42px] font-dm-sans text-[16px] font-normal leading-[20.83px] text-left'><a href="">Blog</a></li>
-                    <li className='w-[44px] h-[19.42px] font-dm-sans text-[16px] font-normal leading-[20.83px] text-left'><a href="">Contact</a></li>
+                <ul className='flex space-x-4'>
+                    <li className='font-dm-sans text-[16px] font-normal leading-[20.83px]'>
+                        <a href="">Home</a>
+                    </li>
+                    <li className='font-dm-sans text-[16px] font-normal leading-[20.83px]'>
+                        <a href="">Services</a>
+                    </li>
+                    <li className='font-dm-sans text-[16px] font-normal leading-[20.83px]'>
+                        <a href="">About Us</a>
+                    </li>
+                    <li className='font-dm-sans text-[16px] font-normal leading-[20.83px]'>
+                        <a href="">Blog</a>
+                    </li>
+                    <li className='font-dm-sans text-[16px] font-normal leading-[20.83px]'>
+                        <a href="">Contact</a>
+                    </li>
                 </ul>
+
            </div>
         </div>
     </>
   )
 }
-
 export default Header
